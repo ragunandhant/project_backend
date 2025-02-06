225c4adbaea8f8628503b195960d6b0a59378834
sreeragunandha <sreeragunandha.t@akaiketech.com>
2025-02-06 21:38:15 +0530

    m

---
+++ src/app/api/race/[raceid]/route.ts
@@ -0,0 +1,40 @@
+
+import { NextResponse } from "next/server";
+import db from "@/db/connection";
+import { categories } from "@/db/schema";
+import { eq } from "drizzle-orm";
+
+
+export async function GET(
+    request: Request,
+    { params }: { params: { raceid: string } }
+  ){
+
+    try {
+        const allEntries = await db.select().from(categories).where(eq(categories.id,params.raceid))
+        return NextResponse.json(
+            {
+                "message": allEntries.length > 0 ? "entries found" : "no entries found",
+                "error":false,
+                "error_message":null,
+                "data":allEntries
+            },{
+                status:200
+            }
+        )  
+
+        
+    } catch (error) {
+        
+        return NextResponse.json(
+            {
+                "message":"",
+                "error":true,
+                "error_message":error,
+            },{
+                status:500
+
+    }
+    )
+  }
+}
